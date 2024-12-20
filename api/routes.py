2a34528a9743d1f275da20b31e760747f1b2e4f9
Kiretori <kiretori2003@gmail.com>
2024-12-20 22:14:28 +0100

    login page

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from ..database.connect_db import get_db_connection
@@ -13,13 +13,11 @@ db_dependency = Annotated[Session, Depends(get_db_connection)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
-@router.get("/")
-async def user(user: user_dependency, db: db_dependency):
-    if user is None:
-        raise HTTPException(status_code=401, detail="Authentication failed")
-    return {"User": user}
-
-
 @router.get("/admin-only")
 async def admin_route(current_user: Annotated[dict, Depends(get_current_admin_user)]):
     return {"message": "Admin access granted", "user": current_user}
+
+
+@router.get("/")
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
